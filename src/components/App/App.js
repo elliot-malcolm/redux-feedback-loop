1ef17911537337f771f55124d87c20c457b62ab2
Elliot Malcolm <talcumalc@gmail.com>
2020-11-01 01:03:03 -0500

    feeling component partial and boilerplate styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import {HashRouter as Router, Route} from 'react-router-dom'
 import { connect } from 'react-redux';
 import Feeling from '../Feeling/Feeling';
+import Supported from '../Supported/Supported';
+import Comments from '../Comments/Comments';
+import Understanding from '../Understanding/Understanding';
 
 class App extends Component {
   render() {
@@ -12,8 +15,12 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <h1 className="App-title">Feedback!</h1>
-            <h4><i>Don't forget it!</i></h4>
+            <h4><i>Tell us how we're doin!</i></h4>
           </header>
+          <Feeling/>
+          <Supported/>
+          <Comments/>
+          <Understanding/>
           <br/>
         </div>
         <Route exact path="/" component={Feeling}/>
--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -3,9 +3,26 @@ import {HashRouter as Router, Route} from 'react-router-dom'
 import { connect } from 'react-redux';
 
 class Feeling extends Component {
+
+    //handleChange dispatch 
+    //submitFeels
+
   render() {
     return (
-            <h1> Feeling </h1>
+            <div className="formInput">
+                    <h1>How are you feeling after today?</h1>
+                    <form onSubmit={this.submitFeels}>
+                    <label>
+                        Feeling?
+                    </label>
+                    <p><input 
+                    type="number"
+                    placeholder="Feels (1-5)"
+                    onChange={this.changeDistance}
+                    ></input></p>
+                    <button className="nextButton">Next</button>
+                    </form>
+            </div>
     );
   }
 }
